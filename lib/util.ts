00627efcc2583590fa2aed29e606cb1b64878f28
Derek G <45305531+dGrowl@users.noreply.github.com>
2022-12-23 23:22:57 -0800

    Add endpoint for updating identities

--- lib/util.ts
+++ lib/util.ts
@@ -1,3 +1,6 @@
 export const jsonHeaders = new Headers({ 'Content-Type': 'application/json' })
 
 export const platforms = ['Twitch', 'Twitter'] as const
+
+export const hasKey = (o: object, key: string) =>
+  Object.prototype.hasOwnProperty.call(o, key)
--- pages/api/identities.ts
+++ pages/api/identities.ts
@@ -1,21 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import db from '../../lib/db'
+import { hasKey } from '../../lib/util'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'POST') {
-    const { body } = req
-    const { user_name, platform, platform_name, desc } = body
+const create = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { body } = req
+  const { user_name, platform, platform_name, desc } = body
+  try {
+    await db.query('CALL add_identity($1, $2, $3, $4);', [
+      user_name,
+      platform,
+      platform_name,
+      desc,
+    ])
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({})
+    return
+  }
+  res.status(200).json({})
+}
+
+const update = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { body } = req
+  const { platform, name } = body
+  if (hasKey(body, 'desc')) {
+    const { desc } = body
     try {
-      await db.query('CALL add_identity($1, $2, $3, $4);', [
-        user_name,
-        platform,
-        platform_name,
-        desc,
-      ])
+      await db.query(
+        `
+          UPDATE public.identities i
+          SET description = $1
+          WHERE platform = $2
+            AND name = $3;
+        `,
+        [desc, platform, name]
+      )
     } catch (error) {
       console.log(error)
       res.status(500).json({})
@@ -23,4 +43,20 @@ export default async function handler(
     }
   }
   res.status(200).json({})
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case 'POST':
+      create(req, res)
+      break
+    case 'PATCH':
+      update(req, res)
+      break
+    default:
+      res.status(405).json({})
+  }
 }
