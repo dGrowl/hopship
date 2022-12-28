f68825ff77c039814e4599702f2aff5e9088375a
Derek G <45305531+dGrowl@users.noreply.github.com>
2022-12-28 14:27:41 -0800

    Add numeric clamp function

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -4,3 +4,6 @@ export const platforms = ['Twitch', 'Twitter'] as const
 
 export const hasKey = (o: object, key: string) =>
   Object.prototype.hasOwnProperty.call(o, key)
+
+export const clamp = (x: number, lower: number, upper: number) =>
+  Math.min(Math.max(x, lower), upper)
