f390b43c69a01b15220c51f6678ed321e1ad2d0f
Derek G <45305531+dGrowl@users.noreply.github.com>
2023-03-20 08:22:43 -0700

    Add unverified and editable controls

--- src/components/IdentityBox.tsx
+++ src/components/IdentityBox.tsx
@@ -4,16 +4,28 @@ interface Props {
   platform: string
   name: string
   desc: string
+  verified?: boolean
+  editable?: boolean
 }
 
-const IdentityBox = ({ platform, name, desc }: Props) => {
+const IdentityBox = ({ platform, name, desc, verified, editable }: Props) => {
   return (
     <div className={`${styles.container} ${styles[platform + 'Border']}`}>
       <div className={`${styles.platform} ${styles[platform]}`}>{platform}</div>
       <div className={styles.details}>
-        <h3>{name}</h3>
+        <div className={styles.nameRow}>
+          <h3>{name}</h3>
+          {verified === false ? (
+            <div className={styles.unverified}>UNVERIFIED</div>
+          ) : null}
+        </div>
         {desc.length > 0 ? <div>{desc}</div> : null}
       </div>
+      {editable ? (
+        <div className={styles.buttonColumn}>
+          <button>C</button>
+        </div>
+      ) : null}
     </div>
   )
 }
